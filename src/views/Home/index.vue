ae6a148e95292afc4f4b80ff614933b355be7d9e
yioqoqyioqoq1 <yioqoqyioqoq@gmail.com>
2025-12-23 20:41:31 +0800

    面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,7 +3,8 @@ import HomeCategory from './components/HomeCategory.vue'
 import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
-import homeProduct from './components/HomeProduct.vue'
+import HomeProduct from './components/HomeProduct.vue'
+import Homepanel from './components/Homepanel.vue'
 </script>
 
 <template>
@@ -14,4 +15,18 @@ import homeProduct from './components/HomeProduct.vue'
   <HomeNew />
   <HomeHot />
   <homeProduct />
+
+  <!-- 测试 -->
+   <Homepanel title="xinxian" sub-title="xinxian">
+
+    <div> 
+      woshichacao
+    </div>
+   </Homepanel>
+
+    <Homepanel title="reniqi" sub-title="renqi">
+          <div> 
+      woshichacao
+    </div>
+   </Homepanel>
 </template>
