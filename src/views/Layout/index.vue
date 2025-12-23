6702d655efd807b1b61d67d69a7a2083cfa68de0
yioqoqyioqoq1 <yioqoqyioqoq@gmail.com>
2025-12-23 16:55:08 +0800

    路由配置

---
+++ src/views/Layout/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <router-link to="/">首页</router-link>
+        <router-link to="/category">分类</router-link>
+        <router-link to="/login">登录</router-link>
+      </nav>
+    </header>
+    
+
+    <main>
+      <router-view></router-view>
+    </main>
+    
+
+    <footer>
+      <p>底部信息</p>
+    </footer>
+  </div>
+</template>
---
+++ src/views/Login/index.vue
@@ -0,0 +1,4 @@
+<template>
+  我是登录
+  
+</template>
