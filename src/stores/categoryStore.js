3fbb6bfc9566b61b3d0c2d46533783043684cf62
yioqoqyioqoq1 <yioqoqyioqoq@gmail.com>
2025-12-23 20:10:38 +0800

    banner轮播图实现

--- src/stores/categoryStore.js
+++ src/stores/categoryStore.js
@@ -1,12 +1,13 @@
 
 import { ref } from 'vue'
-  import { getCategoryAPI } from '@/apis/layout';
+import { getCategoryAPI } from '@/apis/layout';
 import { defineStore } from 'pinia';
 
 export const useCategoryStore = defineStore('category', () => {
 
 
 const categoryList = ref([])
+
 const getCategory = async()=>{
 const res =  await getCategoryAPI()
 console.log(res)
