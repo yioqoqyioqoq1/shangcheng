8015d41340a891dbf977864d78a6afb826856fe6
yioqoqyioqoq1 <yioqoqyioqoq@gmail.com>
2025-12-23 17:39:52 +0800

    layout组件结构搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,21 +1,12 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
+
 <template>
-  <div>
-    <header>
-      <nav>
-        <router-link to="/">首页</router-link>
-        <router-link to="/category">分类</router-link>
-        <router-link to="/login">登录</router-link>
-      </nav>
-    </header>
-    
-
-    <main>
-      <router-view></router-view>
-    </main>
-    
-
-    <footer>
-      <p>底部信息</p>
-    </footer>
-  </div>
+  <LayoutNav />
+  <LayoutHeader />
+  <router-view />
+  <LayoutFooter />
 </template>
