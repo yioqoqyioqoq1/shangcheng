7a92512d9677e53cce4ed6ac831c7637b2727a46
yioqoqyioqoq1 <yioqoqyioqoq@gmail.com>
2025-12-23 23:55:25 +0800

    导航激活设置 分类列表渲染

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -4,6 +4,7 @@ import { getCategoryAPI } from '@/apis/category';
 import { getBannerAPI } from '@/apis/home';
 import { onMounted, ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
+import GoodsItems from '../Home/components/GoodsItems.vue';
 
  const categoryData = ref({})
   const route = useRoute()
@@ -53,6 +54,28 @@ onMounted(() => getBanner())
       </el-carousel-item>
     </el-carousel>
   </div>
+
+  <!-- 分类数据 -->
+<div class="sub-list">
+  <h3>全部分类</h3>
+  <ul>
+    <li v-for="i in categoryData.children" :key="i.id">
+      <RouterLink to="/">
+        <img :src="i.picture" />
+        <p>{{ i.name }}</p>
+      </RouterLink>
+    </li>
+  </ul>
+</div>
+<div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+  <div class="head">
+    <h3>- {{ item.name }}-</h3>
+  </div>
+  <div class="body">
+    <GoodsItems v-for="good in item.goods" :goods="good" :key="good.id" />
+  </div>
+</div>
+
     </div>
   </div>
 </template>
