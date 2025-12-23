394685ca19a5b910df8a5ae094a5005b00a315e8
yioqoqyioqoq1 <yioqoqyioqoq@gmail.com>
2025-12-23 23:43:26 +0800

    分类banner渲染

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 
 import { getCategoryAPI } from '@/apis/category';
+import { getBannerAPI } from '@/apis/home';
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
 
@@ -12,6 +13,17 @@ import { useRoute } from 'vue-router';
     categoryData.value = res.result
   }
 onMounted(()=>getCategory())
+
+
+const bannerList = ref([])
+const getBanner = async () => {
+  const res = await getBannerAPI({distributionSite:"2"})
+  console.log(res)
+  bannerList.value = res.result
+}
+
+onMounted(() => getBanner())
+
 </script>
 
 <template>
@@ -24,12 +36,32 @@ onMounted(()=>getCategory())
        <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
+
+      <!-- 轮播图 -->
+ <div class="home-banner">
+    <el-carousel height="500px">
+      <el-carousel-item v-for="item in bannerList" :key="item.id">
+        <img :src="item.imgUrl" alt="">
+      </el-carousel-item>
+    </el-carousel>
+  </div>
     </div>
   </div>
 </template>
 
 
 <style scoped lang="scss">
+  
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  margin:0 auto
+
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
 .top-category {
   h3 {
     font-size: 28px;
@@ -107,4 +139,5 @@ onMounted(()=>getCategory())
     padding: 25px 0;
   }
 }
+
 </style>
