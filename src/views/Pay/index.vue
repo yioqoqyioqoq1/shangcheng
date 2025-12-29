66fa904cc1bea4db160a25d9d06450056c281f93
yioqoqyioqoq1 <yioqoqyioqoq@gmail.com>
2025-12-29 16:11:21 +0800

    获取基础支付信息d

--- src/views/Pay/index.vue
+++ src/views/Pay/index.vue
@@ -1,7 +1,17 @@
 
 <script setup>
+import { getOrderAPI } from '@/apis/pay';
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+const route = useRoute()
+const payInfo = ref({})
 
-const payInfo = {}
+const getPayInfo=async ()=>{
+  const res = await getOrderAPI(route.query.id)
+  payInfo.value=res.result
+
+}
+onMounted(()=>getPayInfo())
 </script>
 
 
