c9bc2d7d9dfde7d51cc9b079563942549dd10acc
yioqoqyioqoq1 <yioqoqyioqoq@gmail.com>
2025-12-29 18:37:14 +0800

    tab切换实现

--- src/apis/order.js
+++ src/apis/order.js
@@ -1,7 +1,7 @@
 import http from "@/utils/http";
 /*
 params: {
-	orderState:0,
+	orderState:0, 决定当前显示哪个字段 比如是0时显示的是全部订单。1显示代付款。2显示待发货
   page:1,
   pageSize:2
 }
--- src/views/Member/components/UserOrder.vue
+++ src/views/Member/components/UserOrder.vue
@@ -30,13 +30,18 @@ const getOrderList = async () => {
 }
 onMounted(() => getOrderList())
 
+const tabChange=(type)=>{
+console.log(type)
+params.value.orderState=type
+getOrderList()
+}
 </script>
 
 <template>
   <div class="order-container">
-    <el-tabs>
+    <el-tabs @tab-change="tabChange">
       <!-- tab切换 -->
-      <el-tab-pane v-for="item in tabTypes" :key="item.name" :label="item.label" />
+      <el-tab-pane v-for="item in tabTypes" :key="item.name" :label="item.label"  />
 
       <div class="main-container">
         <div class="holder-container" v-if="orderList.length === 0">
