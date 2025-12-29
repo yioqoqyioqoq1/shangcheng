146bf0a677cb868ee83ca27c59d0e2c00ae57113
yioqoqyioqoq1 <yioqoqyioqoq@gmail.com>
2025-12-29 16:21:06 +0800

    实现支付功能

--- src/views/Pay/index.vue
+++ src/views/Pay/index.vue
@@ -12,6 +12,15 @@ const getPayInfo=async ()=>{
 
 }
 onMounted(()=>getPayInfo())
+
+//跳转支付
+// 支付地址
+// 支付地址
+const baseURL = 'http://pcapi-xiaotuxian-front-devtest.itheima.net/'
+const backURL = 'http://127.0.0.1:5173/paycallback'
+const redirectUrl = encodeURIComponent(backURL)
+const payUrl = `${baseURL}pay/aliPay?orderId=${route.query.id}&redirect=${redirectUrl}`
+
 </script>
 
 
